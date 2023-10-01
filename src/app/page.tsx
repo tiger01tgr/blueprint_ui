e7e53c30852a40f0456a8a7448be0b1e58d6659d
Tiger Li <tiger01tgr@gmail.com>
2023-10-01 02:14:02 -0500

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import styles from './page.module.css'
 import HomePage from '@/components/home/HomePage'
 
+// comment
+
 export default function Home() {
   return (
     <div className={styles.liner}>
