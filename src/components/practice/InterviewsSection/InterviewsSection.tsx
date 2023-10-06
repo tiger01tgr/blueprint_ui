7b0ae443727a9ea97348c74c9e85889a6dec7f35
Aaron <aaronli5260@gmail.com>
2023-10-06 17:46:00 -0500

    jobs page, mobile

--- src/components/practice/InterviewsSection/InterviewsSection.tsx
+++ src/components/practice/InterviewsSection/InterviewsSection.tsx
@@ -1,48 +1,48 @@
 import InterviewCard from '@/components/shared/cards/InterviewCard/InterviewCard'
 import styles from './InterviewsSection.module.css'
 
-const InterviewsSection = () => {
-    const Interviews:any[] = [
-        {
-            id: "MS23T",
-            employerName: "Morgan Stanley",
-            employerLogo: "http://via.placeholder.com/100x100",
-            position: "Summer 2024 Fixed Income Summer Analyst",
-            type: "Technical"
-        },
-        {
-            id: "OKTA23T",
-            employerName: "Okta",
-            employerLogo: "http://via.placeholder.com/100x100",
-            position: "Summer 2024 Software Engineering Intern",
-            type: "Technical"
-        },
-        {
-            id: "OKTA23B",
-            employerName: "Okta",
-            employerLogo: "http://via.placeholder.com/100x100",
-            position: "Summer 2024 Software Engineering Intern",
-            type: "Behavioral"
-        },
-        {
-            id: "GS23T",
-            employerName: "Goldman Sachs",
-            employerLogo: "http://via.placeholder.com/100x100",
-            position: "Summer Analyst",
-            type: "Technical"
-        },
-        {
-            id: "GOOGL23B",
-            employerName: "Google",
-            employerLogo: "http://via.placeholder.com/100x100",
-            position: "Associate Product Manager",
-            type: "Behavioral"
-        }
-    ]
+const interviews: any[] = [
+    {
+        id: "MS23T",
+        employerName: "Morgan Stanley",
+        employerLogo: "http://via.placeholder.com/100x100",
+        position: "Summer 2024 Fixed Income Summer Analyst",
+        type: "Technical"
+    },
+    {
+        id: "OKTA23T",
+        employerName: "Okta",
+        employerLogo: "http://via.placeholder.com/100x100",
+        position: "Summer 2024 Software Engineering Intern",
+        type: "Technical"
+    },
+    {
+        id: "OKTA23B",
+        employerName: "Okta",
+        employerLogo: "http://via.placeholder.com/100x100",
+        position: "Summer 2024 Software Engineering Intern",
+        type: "Behavioral"
+    },
+    {
+        id: "GS23T",
+        employerName: "Goldman Sachs",
+        employerLogo: "http://via.placeholder.com/100x100",
+        position: "Summer Analyst",
+        type: "Technical"
+    },
+    {
+        id: "GOOGL23B",
+        employerName: "Google",
+        employerLogo: "http://via.placeholder.com/100x100",
+        position: "Associate Product Manager",
+        type: "Behavioral"
+    }
+]
 
+const InterviewsSection = () => {
     return (
         <div className={styles.liner}>
-            {Interviews.map((interview) => (
+            {interviews.map((interview) => (
                 <InterviewCard key={interview.id} {...interview} />
             ))}
         </div>
--- src/components/practice/PracticePage.tsx
+++ src/components/practice/PracticePage.tsx
@@ -1,3 +1,4 @@
+'use client'
 import InterviewsSection from './InterviewsSection/InterviewsSection'
 import PracticeBanner from './PracticeBanner/PracticeBanner'
 import styles from './page.module.css'
