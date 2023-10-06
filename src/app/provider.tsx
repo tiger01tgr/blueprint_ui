bafacc01c38609e6cbf458fb82cbc6f497d22d8d
Aaron <aaronli5260@gmail.com>
2023-10-06 00:13:15 -0500

    remove nextui provider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,15 +1,12 @@
 // app/providers.tsx
 'use client'
 import '@mantine/core/styles.css';
-import { NextUIProvider } from '@nextui-org/react'
 import { MantineProvider } from '@mantine/core';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <MantineProvider>
-      <NextUIProvider>
-        {children}
-      </NextUIProvider>
+      {children}
     </MantineProvider>
   )
 }
