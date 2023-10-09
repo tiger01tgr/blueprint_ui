24fb713d82d031446f5a1ada8b509f29d1bafc7f
Aaron <aaronli5260@gmail.com>
2023-10-09 01:11:35 -0500

    privacy

---
+++ src/components/privacy/PrivacyPage.tsx
@@ -0,0 +1,160 @@
+import styles from './PrivacyPage.module.css'
+
+const PrivacyPage = () => {
+    return (
+        <div className={styles.liner}>
+            <div className={styles.title}>
+                Privacy Policy
+            </div>
+            <div className={styles.lastUpdated}>
+                Last Updated: October 08, 2023
+            </div>
+            <div className={styles.termsDescription}>
+                At Bluprint, accessible from joinbluprint.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Bluprint and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us. This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Bluprint
+            </div>
+            <div className={styles.termsLiner}>
+                <div className={styles.termsSection}>
+                    <div className={styles.termsTitle}>
+                        Consent
+                    </div>
+                    <div className={styles.termsDescriptionSection}>
+                        <div className={styles.termsDescription}>
+                            By using our website, you hereby consent to our Privacy Policy and agree to its terms.
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.termsSection}>
+                    <div className={styles.termsTitle}>
+                        Information Collected
+                    </div>
+                    <div className={styles.termsDescriptionSection}>
+                        <div className={styles.termsDescription}>
+                            We collect personal information that you voluntarily provide to us when registering at the Services expressing an interest in obtaining information about us or our products and services, when participating in activities on the Services or otherwise contacting us.
+
+                            The personal information that we collect depends on the context of your interactions with us and the Services, the choices you make and the products and features you use. The personal information we collect can include the following:
+
+                            Publicly Available Personal Information. We collect first name, maiden name, last name, and nickname ; phone numbers ; email addresses ; social media ; and other similar data.
+
+                            Personal Information Provided by You. We collect CV and other job application data such as your education and employment history, background checks, self-reported skills and interests ; and other similar data. We collect any interviews you submit to us.
+
+                            Social Media Login Data. We may provide you with the option to register using social media account.
+
+                            All personal information that you provide to us must be true, complete and accurate, and you must notify us of any changes to such personal information. We automatically collect certain information when you visit, use or navigate the Services. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our Services and other technical information. This information is primarily needed to maintain the security and operation of our Services, and for our internal analytics and reporting purposes. We use Google Analytics to help maintain and improve the operation of the Services.
+
+                            Like many businesses, we also collect information through cookies and similar technologies.
+
+                            Online Identifiers. We collect devices; applications; tools and protocols, such as IP (Internet Protocol) addresses; cookie identifiers, or others such as the ones used for analytics and marketing; device&apos;s geolocation; and other similar data.
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.termsSection}>
+                    <div className={styles.termsTitle}>
+                        How Your Information is Used or Shared
+                    </div>
+                    <div className={styles.termsDescriptionSection}>
+                        <div className={styles.termsDescription}>
+                            We use personal information collected via our Services for a variety of business purposes described below. We process your personal information for these purposes in reliance on our legitimate business interests, in order to enter into or perform a contract with you, with your consent, and/or for compliance with our legal obligations. We indicate the specific processing grounds we rely on next to each purpose listed below.
+
+                            We use the information we collect or receive:
+
+                            To facilitate account creation and logon process. If you choose to link your account with us to a third party account (such as your Google or Facebook account), we use the information you allowed us to collect from those third parties to facilitate account creation and logon process for the performance of the contract.
+                            To send you marketing and promotional communications. We and/or our third party marketing partners may use the personal information you send to us for our marketing purposes, if this is in accordance with your marketing preferences. You can opt-out of our marketing emails at any time.
+                            To send administrative information to you. We may use your personal information to send you product, service and new feature information and/or information about changes to our terms, conditions, and policies.
+                            Fulfill and manage your orders. We may use your information to fulfill and manage your orders, payments, returns, and exchanges made through the Services.
+                            To post testimonials. We post testimonials on our Services that may contain personal information. Prior to posting a testimonial, we will obtain your consent to use your name and testimonial.
+                            Deliver targeted advertising to you. We may use your information to develop and display content and advertising (and work with third parties who do so) tailored to your interests and/or location and to measure its effectiveness.
+                            Request Feedback. We may use your information to request feedback and to contact you about your use of our Services.
+                            To protect our Services. We may use your information as part of our efforts to keep our Services safe and secure (for example, for fraud monitoring and prevention).
+                            To enforce our terms, conditions and policies for Business Purposes, Legal Reasons and Contractual.
+                            To respond to legal requests and prevent harm. If we receive a subpoena or other legal request, we may need to inspect the data we hold to determine how to respond.
+                            To manage user accounts. We may use your information for the purposes of managing our account and keeping it in working order.
+                            To deliver services to the user. We may use your information to provide you with the requested service.
+                            To respond to user inquiries/offer support to users. We may use your information to respond to your inquiries and solve any potential issues you might have with the use of our Services.
+                            For other Business Purposes. We may use your information for other Business Purposes, such as data analysis, identifying usage trends, determining the effectiveness of our promotional campaigns and to evaluate and improve our Services, products, marketing and your experience. We may use and store this information in aggregated and anonymized form so that it is not associated with individual end users and does not include personal information. We will not use identifiable personal information without your consent.
+                        </div>
+                        <div className={styles.termsDescription}>
+                            We may process your data if you have given us specific consent to use your personal information in a specific purpose.
+                            Legitimate Interests: We may process your data when it is reasonably necessary to achieve our legitimate business interests.
+                            Performance of a Contract: Where we have entered into a contract with you, we may process your personal information to fulfill the terms of our contract.
+                            Legal Obligations: We may disclose your information where we are legally required to do so in order to comply with applicable law, governmental requests, a judicial proceeding, court order, or legal process, such as in response to a court order or a subpoena (including in response to public authorities to meet national security or law enforcement requirements).
+                            Vital Interests: We may disclose your information where we believe it is necessary to investigate, prevent, or take action regarding potential violations of our policies, suspected fraud, situations involving potential threats to the safety of any person and illegal activities, or as evidence in litigation in which we are involved.
+                            More specifically, we may need to process your data or share your personal information in the following situations:
+
+                            Vendors, Consultants and Other Third-Party Service Providers. We may share your data with third party vendors, service providers, contractors or agents who perform services for us or on our behalf and require access to such information to do that work. Examples include: payment processing, data analysis, email delivery, hosting services, customer service and marketing efforts. We may allow selected third parties to use tracking technology on the Services, which will enable them to collect data about how you interact with the Services over time. This information may be used to, among other things, analyze and track data, determine the popularity of certain content and better understand online activity. Unless described in this Policy, we do not share, sell, rent or trade any of your information with third parties for their promotional purposes.
+                            Business Transfers. We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company.
+                            Third-Party Advertisers. We may use third-party advertising companies to serve ads when you visit the Services. These companies may use information about your visits to our Website(s) and other websites that are contained in web cookies and other tracking technologies in order to provide advertisements about goods and services of interest to you.
+                            Affiliates. We may share your information with our affiliates, in which case we will require those affiliates to honor this privacy policy . Affiliates include our parent company and any subsidiaries, joint venture partners or other companies that we control or that are under common control with us.
+                            Business Partners. We may share your information with our business partners to offer you certain products, services or promotions.
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.termsSection}>
+                    <div className={styles.termsTitle}>
+                        Advertising Partners Privacy Policies
+                    </div>
+                    <div className={styles.termsDescriptionSection}>
+                        <div className={styles.termsDescription}>
+                            Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Bluprint, which are sent directly to users&apos; browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+
+                            Note that Bluprint has no access to or control over these cookies that are used by third-party advertisers.
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.termsSection}>
+                    <div className={styles.termsTitle}>
+                        Third Party Privacy Policies
+                    </div>
+                    <div className={styles.termsDescriptionSection}>
+                        <div className={styles.termsDescription}>
+                            Bluprint&apos;s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
+
+                            You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers&apos; respective websites.
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.termsSection}>
+                    <div className={styles.termsTitle}>
+                        CCPA Privacy Rights
+                    </div>
+                    <div className={styles.termsDescriptionSection}>
+                        <div className={styles.termsDescription}>
+                            Under the CCPA, among other rights, California consumers have the right to:
+
+                            Request that a business that collects a consumer&apos;s personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
+
+                            Request that a business delete any personal data about the consumer that a business has collected.
+
+                            Request that a business that sells a consumer&apos;s personal data, not sell the consumer&apos;s personal data.
+
+                            If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.termsSection}>
+                    <div className={styles.termsTitle}>
+                        GDPR Data Protection Rights
+                    </div>
+                    <div className={styles.termsDescriptionSection}>
+                        <div className={styles.termsDescription}>
+                            We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
+
+                            The right to access — You have the right to request copies of your personal data. We may charge you a small fee for this service.
+
+                            The right to rectification — You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.
+
+                            The right to erasure — You have the right to request that we erase your personal data, under certain conditions.
+
+                            The right to restrict processing — You have the right to request that we restrict the processing of your personal data, under certain conditions.
+
+                            The right to object to processing — You have the right to object to our processing of your personal data, under certain conditions.
+
+                            If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PrivacyPage
