7aaf0bc65c967352fc35092dc8edfad96c37e925
Aaron <aaronli5260@gmail.com>
2023-10-20 23:18:52 -0500

    Remove console log

--- src/hooks/session/sessionApi.ts
+++ src/hooks/session/sessionApi.ts
@@ -33,7 +33,6 @@ export const createSessionWithId = async (token: string, questionSetId: number):
             'Authorization': 'Bearer ' + token,
         }
     })
-    console.log(response)
     if (!response.ok) throw new Error('Error occurred')
     return null
 }
